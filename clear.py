d612a9d797da05879fcbe0661ccdbf6512afafb0
mitsuijao <nishima.kun@gmail.com>
2025-09-01 19:18:18 +0900

    v1.0

---
+++ clear.py
@@ -0,0 +1,7 @@
+from e_paper import epd7in5b_V2
+from e_paper import epdconfig
+
+
+epd = epd7in5b_V2.EPD()
+epd.init()
+epd.Clear()
---
+++ client.py
@@ -0,0 +1,33 @@
+from e_paper import epd7in5b_V2
+from e_paper import epdconfig
+import requests
+
+
+epd = epd7in5b_V2.EPD()
+try:
+    size = 480*800 // 8 
+    url = "https://mitsuijao.fun/api/calendar"
+    payload = {}
+    headers = {}
+
+    res = requests.request("GET", url, headers=headers, data=payload)
+
+    if res.status_code == 200:
+        black = res.content[:size]
+        red = res.content[size:]
+
+
+        epd.init()
+        epd.Clear()
+        epd.display(bytearray([255 - b for b in black]), bytearray([255 - r for r in red]))
+    
+    else:
+        print("connection error")
+        print(res.status_code)
+        print(res.text)
+
+except Exception as e:
+    print(e)
+
+finally:
+    epd.sleep()
